15f0a078686c2b9420f5bbc26a069f352a8cea4f
Evgeny Shikanov <sheekanov@gmail.com>
2018-08-02 23:49:43 +0300

    Список Статусов (status) и Режимов (mode) вынесен в статические переменныеи модели Orders

--- models/Order.php
+++ models/Order.php
@@ -5,6 +5,21 @@ use yii\db\ActiveRecord;
 
 class Order extends ActiveRecord
 {
+    static $allStatuses = [
+        'all' => 'All orders',
+        '0' => 'Pending',
+        '1' => 'In Progress',
+        '2' => 'Completed',
+        '3' => 'Canceled',
+        '4' => 'Error'
+    ];
+
+    static $allModes = [
+        'all' => 'All',
+        '0' => 'Manual',
+        '1' => 'Auto'
+    ];
+
     public static function tableName()
     {
         return '{{orders}}';
